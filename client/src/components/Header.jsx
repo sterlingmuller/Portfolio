3c16f448e863dc328ca715c0d10269acd27ed6a9
Sterling Muller <sterlingmuller93@gmail.com>
2022-03-28 02:28:59 -0400

    added '/' between navbar links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,13 +12,13 @@ export const Header = () => {
       <div className='header'>
         <Link to='/home' className='headerLink'>Home
         <div className={`${pathname === '/' || pathname === '/home' ? 'link-indicator-selected' : 'link-indicator'}`}>.</div>
-        </Link>
+        </Link> /
         <Link to='/projects' className='headerLink'>Projects
         <div className={`${pathname === '/projects' ? 'link-indicator-selected' : 'link-indicator'}`}>.</div>
-        </Link>
+        </Link> /
         <Link to='/music' className='headerLink'>Music
         <div className={`${pathname === '/music' ? 'link-indicator-selected' : 'link-indicator'}`}>.</div>
-        </Link>
+        </Link> /
         <Link to='/connect' className='headerLink'>Connect
         <div className={`${pathname === '/connect' ? 'link-indicator-selected' : 'link-indicator'}`}>.</div>
         </Link>
