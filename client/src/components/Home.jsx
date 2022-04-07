8f85f4bbf80f6900a4ef23d90558e612275ca30b
Sterling Muller <sterlingmuller93@gmail.com>
2022-04-07 00:04:12 -0400

    expanding music cards

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Home = () => {
+  const [wiggle, setWiggle] = useState(false);
+
+  function handleClick () {
+    setWiggle(true);
+    setTimeout(() => setWiggle(false), 1000);
+  }
 
   return (
     <div id='about'>
       <div className="container">
-      <img src={'./photo.jpg'} className="photo"></img>
+      <img src={'./photo.jpg'} className={wiggle ? "photo wiggle" : "photo"}></img>
       </div>
       <div className="bio">
-      <p>Welcome to my home. Take off your shoes and stay awhile.
+      <p>Welcome to my
+        <span style={{ 'color': '#f3989e', 'fontWeight': 'bold'}}> home</span>
+        . Take off your shoes and stay awhile.
       </p>
       <p>
       My name is
-      <span className='home-link'> Sterling Muller</span>. <br/>
+      <span className='home-link' onClick={() => handleClick()}> Sterling Muller</span>. <br/>
       I am fullstack software engineer, with a particular interest in frontend development,
         looking to build up communities through meaningful and well-designed applications.
       </p>
--- client/src/components/Music.jsx
+++ client/src/components/Music.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Music = () => {
 
+
   return (
-    <div className='music-videos'>
-      <iframe className='vid-left' width="560" height="315" src="https://www.youtube.com/embed/vjk7s6ktBTs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <iframe className='vid-right' width="560" height="315" src="https://www.youtube.com/embed/MyJInCv4viU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <iframe title="vimeo-player" className='vid-left' src="https://player.vimeo.com/video/222915625?h=24371e9963" width="560" height="315" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  allowfullscreen></iframe>
-      <iframe className='vid-right' width="560" height="315" src="https://www.youtube.com/embed/DMV5KWYV95A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+    <div className="card-grid">
+
+        <div className='left-card'>
+          <iframe className='vid' width="560" height="315" src="https://www.youtube.com/embed/vjk7s6ktBTs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen onMouseEnter={() => handleHover('ghoul')}></iframe>
+          <span className="music-body">
+          <h2 className="title">Ghoul Friends</h2>
+          <p> This claymation I made is the perfect intersection of my love for playing music and crafting.
+          </p>
+        </span>
+        </div>
+
+        <div className='right-card'>
+          <iframe className='vid' width="560" height="315" src="https://www.youtube.com/embed/MyJInCv4viU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <div className="music-body">
+          <h2 className="title">Atelier Backend System Design</h2>
+          <p>Atelier System
+          </p>
+        </div>
+        </div>
+
+        <div className='left-vimeo'>
+        <iframe title="vimeo-player" className='vid' src="https://player.vimeo.com/video/222915625?h=24371e9963" width="640" height="320" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  allowfullscreen></iframe>
+        <div className="music-body">
+          <h2 className="title">Atelier Backend System Design</h2>
+          <p>Atelier System Design
+          </p>
+        </div>
+        </div>
+
+        <div className='right-card'>
+        <iframe className='vid' width="560" height="315" src="https://www.youtube.com/embed/DMV5KWYV95A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen></iframe>
+        <div className="music-body">
+          <h2 className="title">Atelier Backend System Design</h2>
+          <p>Atelier System Design
+          </p>
+        </div>
+        </div>
+
     </div>
   )
 }
