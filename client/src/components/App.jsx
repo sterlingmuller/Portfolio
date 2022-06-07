cde47d9e517546e468ec3db1be193a3d75f2fab4
Sterling Muller <sterlingmuller93@gmail.com>
2022-06-07 01:19:42 -0400

    added scaling for mobile devices

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RouteComponent from '../RouteComponent.jsx';
-import { MainContext } from '../MainContext.js';
 
 const App = () => {
 
 
   return (
     <div className='main'>
-      <MainContext.Provider value={{
-
-      }}>
-        <RouteComponent />
-      </MainContext.Provider>
+      <RouteComponent />
     </div>
   );
 }
